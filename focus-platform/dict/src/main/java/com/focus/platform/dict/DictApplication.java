5364d1c18a40257d05341b66fc6eee442d8c9435
liuxianglei@jiwu.com <liuxianglei@jiwu.com>
2019-08-13 19:23:40 +0800

    修改

--- focus-platform/dict/src/main/java/com/focus/platform/dict/DictApplication.java
+++ focus-platform/dict/src/main/java/com/focus/platform/dict/DictApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableMybatisType
+@EnableMybatisType(MybatisType.PLUS)
 public class DictApplication {
 
     public static void main(String[] args) {
